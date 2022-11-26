2f62b79fd5a087a801539be6faf61ee10c222979
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-11-25 21:16:26 -0500

    FEAT: Amazon Gateway Proxy.

--- src/AWS-API-Gateway/Function.cs
+++ src/AWS-API-Gateway/Function.cs
@@ -1,4 +1,6 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace GatewayLambda.Demo
@@ -7,19 +9,25 @@ namespace GatewayLambda.Demo
      Deploy:
      dotnet lambda deploy-function LambdaDemo
 
+    GateWay Link
+    https://gn7pzmiiwl.execute-api.us-east-1.amazonaws.com/prod
+
      */
     public class Function
     {
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            context.Logger.LogLine($"Executing API Gateway.");
-            return "Helo World";
+            string name = "No-Name";
+            if(request.QueryStringParameters != null && request.QueryStringParameters.ContainsKey("name")) {
+                name = request.QueryStringParameters["name"];
+            }
+            context.Logger.LogLine($"Got name {name}");
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = $"Use5r name passed : {name}",
+                IsBase64Encoded = false,
+            };
         }
     }
 }
